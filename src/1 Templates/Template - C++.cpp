321d6fcdf8f981e6d833b72d6d75b31327f287c0
Albert Gural <ag@albertgural.com>
2016-05-07 03:47:18 -0700

    Added notebook library code.

---
+++ src/1 Templates/Template - C++.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+static bool DBG = 1;
+
+ll mod(ll a, ll b) {
+    return ((a%b)+b)%b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cout << fixed << setprecision(15);
+	int n;
+	cin >> n;
+	cout << n << endl;
+    return 0;
+}
---
+++ src/2 Data Structures/Union Find.cpp
@@ -0,0 +1,25 @@
+// (struct) also keeps track of sizes
+struct union_find {
+	vector<int> P,S;
+
+	union_find(int N) {
+		P.resize(N), S.resize(N, 1);
+		for(int i = 0; i < N; i++) P[i] = i;
+	}
+
+	int rep(int i) {
+		return (P[i] == i) ? i : P[i] = rep(P[i]);
+	}
+
+	bool unio(int a, int b) {
+		a = rep(a), b = rep(b);
+		if(a == b) return false;
+		P[b] = a;
+		S[a] += S[b];
+		return true;
+	}
+};
+
+// (Shorter) union-find set: the vector/array contains the parent of each node
+int find(vector <int>& C, int x){return (C[x]==x) ? x : C[x]=find(C, C[x]);} //C++
+int find(int x){return (C[x]==x)?x:C[x]=find(C[x]);} //C
